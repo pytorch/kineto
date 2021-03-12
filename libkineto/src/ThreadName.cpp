4f559fcc2e999adc0647c86f2c8c7ae3fb600464
Gisle Dankel <gdankel@fb.com>
2021-03-11 21:00:09 -0800

    Change to safe thread name implementation
    Summary:
    Passing an invalid thread handle to pthread_getname_np may crash the process. This can happen when we try to get the name for a thread that has exited.
    
    Switch to a robust implementation - which unfortunately is linux specific.
    We can re-evaluate this later - another approach is to get the thread names from within the threads themselves as they are logging events.
    
    Differential Revision: D27003592
    
    fbshipit-source-id: b4d4bcd34f8a6d61e8d12581c91c39d8d9a18d40

--- libkineto/src/ThreadName.cpp
+++ libkineto/src/ThreadName.cpp
@@ -5,9 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <string>
-
+#include <fmt/format.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <string>
+#include <unistd.h>
+
+#include "Logger.h"
 
 namespace KINETO_NAMESPACE {
 
@@ -17,12 +21,21 @@ bool setThreadName(const std::string& name) {
   return 0 == pthread_setname_np(pthread_self(), name.c_str());
 }
 
-std::string getThreadName(pthread_t id) {
-  char buf[kMaxThreadNameLength];
-  if (0 == pthread_getname_np(id, buf, sizeof(buf))) {
-    return buf;
+std::string getThreadName(pid_t tid) {
+  char buf[kMaxThreadNameLength] = "Unknown";
+  std::string filename = fmt::format("/proc/{}/task/{}/comm", getpid(), tid);
+  FILE* comm_file = fopen(filename.c_str(), "r");
+  if (comm_file) {
+    size_t len = fread(buf, 1, kMaxThreadNameLength, comm_file);
+    fclose(comm_file);
+    // Remove newline
+    if (len > 0) {
+      buf[len - 1] = '\0';
+    }
+  } else {
+    LOG(WARNING) << "Failed to open " << filename;
   }
-  return "Unknown";
+  return buf;
 }
 
 } // namespace KINETO_NAMESPACE

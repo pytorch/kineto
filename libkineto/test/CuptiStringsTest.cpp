868cf8117b605ba3253ce079016a971b47178036
Gisle Dankel <gdankel@fb.com>
2021-03-16 18:43:17 -0700

    Fix cbid name lookup bounds check and remove static assert (#121)
    Summary:
    Pull Request resolved: https://github.com/pytorch/kineto/pull/121
    
    The static assert adds additional work for PyTorch folks when updating the PyTorch Cuda version.
    
    Note: It was added to notify us when the table needs updating, which is when PyTorch adds support for a new version of Cuda.
    But if not using this approach we need another automatic reminder to add the support.
    
    Also, the bounds check should use the names array size not the enum size, as otherwise it will break when running a version compiled for one version of Cuda on a newer version.
    
    Reviewed By: chaekit
    
    Differential Revision: D27084725
    
    fbshipit-source-id: 36a8d61da5979f9e6c98832cd8abffd426b28673

---
+++ libkineto/test/CuptiStringsTest.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include "src/cupti_strings.h"
+
+using namespace KINETO_NAMESPACE;
+
+TEST(CuptiStringsTest, Valid) {
+  ASSERT_STREQ(
+      runtimeCbidName(CUPTI_RUNTIME_TRACE_CBID_INVALID), "INVALID");
+  ASSERT_STREQ(
+      runtimeCbidName(CUPTI_RUNTIME_TRACE_CBID_cudaDriverGetVersion_v3020),
+      "cudaDriverGetVersion");
+  ASSERT_STREQ(runtimeCbidName
+      (CUPTI_RUNTIME_TRACE_CBID_cudaDeviceSynchronize_v3020),
+      "cudaDeviceSynchronize");
+  ASSERT_STREQ(
+      runtimeCbidName(CUPTI_RUNTIME_TRACE_CBID_cudaStreamSetAttribute_ptsz_v11000),
+      "cudaStreamSetAttribute_ptsz");
+}
+
+TEST(CuptiStringsTest, Invalid) {
+  ASSERT_STREQ(runtimeCbidName(-1), "INVALID");
+  // We can't actually use CUPTI_RUNTIME_TRACE_CBID_SIZE here until we
+  // auto-generate the string table, since it may have more entries than
+  // the enum in the version used to compile.
+  ASSERT_STREQ(runtimeCbidName(1000), "INVALID");
+}

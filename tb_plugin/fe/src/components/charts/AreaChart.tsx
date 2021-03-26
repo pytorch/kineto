a2173b1e8cc7bf1da090f50460b4ae7a5ac79546
Wenlu Wang <wenlwan@microsoft.com>
2021-03-26 17:48:37 +0800

    Resize the graph when window resized

--- tb_plugin/fe/src/components/charts/AreaChart.tsx
+++ tb_plugin/fe/src/components/charts/AreaChart.tsx
@@ -5,6 +5,7 @@
 import { makeStyles } from '@material-ui/core/styles'
 import * as React from 'react'
 import { Graph } from '../../api'
+import { useResizeEventDependency } from '../../utils/resize'
 
 interface IProps {
   graph: Graph
@@ -22,6 +23,7 @@ export const AreaChart: React.FC<IProps> = (props) => {
   const { graph, height = 400, hAxisTitle } = props
   const classes = useStyles({ height })
   const graphRef = React.useRef<HTMLDivElement>(null)
+  const [resizeEventDependency] = useResizeEventDependency()
 
   React.useLayoutEffect(() => {
     const element = graphRef.current
@@ -58,7 +60,7 @@ export const AreaChart: React.FC<IProps> = (props) => {
     return () => {
       chart.clearChart()
     }
-  }, [graph, height])
+  }, [graph, height, resizeEventDependency])
 
   return (
     <div className={classes.root}>

a06b966ddf65742b5440828bde74e7fc86443fc0
Mike Guo <myguo@microsoft.com>
2021-04-22 13:54:07 +0800

    clean setup future import

--- tb_plugin/setup.py
+++ tb_plugin/setup.py
@@ -1,11 +1,6 @@
 # -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # --------------------------------------------------------------------------
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
 import setuptools
 

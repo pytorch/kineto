d95f36c6e32f7eab8466d91c68b94b68dc06f522
Brian Coutinho <bcoutinho@fb.com>
2021-07-06 12:21:16 -0700

    Add interface to register child activity profilers (#324)
    Summary:
    Pull Request resolved: https://github.com/pytorch/kineto/pull/324
    
    First cut for api to register profilers
    * Moved the profiler to a unique_ptr so it is owned by ActivityProfiler object
    * Use of factory style of initializers on libkineto API
    
    Reviewed By: gdankel
    
    Differential Revision: D29385554
    
    fbshipit-source-id: ac342d370c157f0277f495b4cee08b7673c33ff0

--- libkineto/src/ActivityProfilerProxy.h
+++ libkineto/src/ActivityProfilerProxy.h
@@ -63,6 +63,9 @@ class ActivityProfilerProxy : public ActivityProfilerInterface {
 
   void addMetadata(const std::string& key, const std::string& value) override;
 
+  virtual void addChildActivityProfiler(
+      std::unique_ptr<IActivityProfiler> profiler) override;
+
  private:
   bool cpuOnly_{true};
   ConfigLoader& configLoader_;

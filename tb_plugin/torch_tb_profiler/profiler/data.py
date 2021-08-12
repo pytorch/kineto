b8870122f3a1ba4fbda0aadd8ab6f59a973d582c
guyang3532 <62738430+guyang3532@users.noreply.github.com>
2021-08-12 14:33:07 +0800

    Support gloo communication parse (#377)

--- tb_plugin/torch_tb_profiler/profiler/data.py
+++ tb_plugin/torch_tb_profiler/profiler/data.py
@@ -11,7 +11,7 @@ from json.decoder import JSONDecodeError
 from .. import io, utils
 from . import trace
 from .communication import analyze_communication_nodes
-from .event_parser import EventParser, ProfileRole
+from .event_parser import EventParser, ProfileRole, CommLibTypes
 from .gpu_metrics_parser import GPUMetricsParser
 from .kernel_parser import KernelParser
 from .memory_parser import MemoryParser
@@ -30,7 +30,7 @@ class RunProfileData(object):
         self.used_devices = []
         self.use_dp = False
         self.use_ddp =False
-        self.use_nccl = False
+        self.comm_lib = None
         self.events = None
         self.trace_file_path = None
         self.has_runtime = False
@@ -164,7 +164,7 @@ class RunProfileData(object):
         self.used_devices = sorted(list(parser.used_devices))
         self.use_dp = parser.use_dp
         self.use_ddp = parser.use_ddp
-        self.use_nccl = parser.use_nccl
+        self.comm_lib = parser.comm_lib
 
         # Parse communications.
         self.comm_node_list = parser.generate_communication_nodes()
@@ -237,7 +237,7 @@ class RunProfileData(object):
                    )
             self.recommendations.append(text)
 
-        if self.use_ddp and not self.use_nccl and self.device_props:
+        if self.use_ddp and CommLibTypes.Nccl not in self.comm_lib and self.device_props:
             for device_prop in self.device_props:
                 major = device_prop.get("computeMajor")
                 minor = device_prop.get("computeMinor")
@@ -288,6 +288,7 @@ class DistributedRunProfileData:
         self.span = run_profile_data.span
         self.steps_names = run_profile_data.steps_names
         self.has_communication = run_profile_data.has_communication
+        self.comm_lib = run_profile_data.comm_lib
         self.comm_node_list = run_profile_data.comm_node_list
         self.comm_overlap_costs = run_profile_data.comm_overlap_costs
         self.used_devices = run_profile_data.used_devices

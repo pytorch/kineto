fadcd13e813bf6b798189cca48d7790dd9e582c8
Han Guangyun <guangyunhan@microsoft.com>
2021-08-19 18:42:31 +0800

    make mp verbose

--- tb_plugin/torch_tb_profiler/profiler/multiprocessing.py
+++ tb_plugin/torch_tb_profiler/profiler/multiprocessing.py
@@ -1,9 +1,11 @@
 import multiprocessing as mp
 import os
 
+logger = mp.log_to_stderr()
+logger.setLevel(mp.SUBDEBUG)
 
 def get_start_method():
-    return os.getenv('TORCH_PROFILER_START_METHOD', "spawn")
+    return os.getenv('TORCH_PROFILER_START_METHOD')
 
 __all__ = [x for x in dir(mp.get_context(get_start_method())) if not x.startswith('_')]
 globals().update((name, getattr(mp.get_context(get_start_method()), name)) for name in __all__)

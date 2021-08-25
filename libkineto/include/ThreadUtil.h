031f2dee4348fe99488b42dda5d40b72f91d0518
Gisle Dankel <gdankel@fb.com>
2021-08-25 13:50:55 -0700

    Refactor GPU stream name handling (#398)
    Summary:
    Pull Request resolved: https://github.com/pytorch/kineto/pull/398
    
    GPU streams are currently handled specially in the logger. Bring GPU streams in line with thread names. This enables two things:
    1) Streams can now be sorted correctly
    2) Adding generic flow arrows becomes easier and cleaner
    
    Reviewed By: chaekit
    
    Differential Revision: D30523099
    
    fbshipit-source-id: a67e95d1fe1fd696b4b88f6f05a47590781f714d

--- libkineto/include/ThreadUtil.h
+++ libkineto/include/ThreadUtil.h
@@ -15,19 +15,6 @@ std::string getThreadName();
 int32_t processId();
 std::string processName(int32_t pid);
 
-struct ProcessInfo {
-  int32_t pid;
-  const std::string name;
-  const std::string label;
-};
-
-struct ThreadInfo {
-  ThreadInfo(int32_t tid, const std::string& name) :
-    tid(tid), name(name) {}
-  int32_t tid;
-  const std::string name;
-};
-
 // Return a list of pids and process names for the current process
 // and its parents.
 std::vector<std::pair<int32_t, std::string>> pidCommandPairsOfAncestors();

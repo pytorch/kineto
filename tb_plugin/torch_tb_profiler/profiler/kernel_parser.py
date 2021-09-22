b5d2738cb3b22bd746a584d2ca1fbacbf0986f05
Teng Gao <tegao@microsoft.com>
2021-09-22 22:14:58 +0800

    fix a pandas dataframe usage bug

--- tb_plugin/torch_tb_profiler/profiler/kernel_parser.py
+++ tb_plugin/torch_tb_profiler/profiler/kernel_parser.py
@@ -17,7 +17,7 @@ class KernelParser:
         events = [vars(event) for event in events if event.type == EventTypes.KERNEL]
         events = pd.DataFrame(events)
         events = events.astype({"type": "category", "name": "string"}, copy=False)
-        events["tc_used"] = events["name"] in TC_Allowlist
+        events["tc_used"] = events["name"].map(lambda name: name in TC_Allowlist)
 
         def weighted_avg(x):
             try:

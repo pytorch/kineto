41364e788302d000ed8e163d1d06c91ac0425f3e
Brian Coutinho <bcoutinho@fb.com>
2021-11-11 12:57:33 -0800

    Add Config options to configure CUPTI profiler
    Differential Revision: D31971370
    
    fbshipit-source-id: 55680f6f633f64190fc05647beb280191887f49c

---
+++ libkineto/src/CuptiCallbackAPI.h
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <atomic>
+#ifdef HAS_CUPTI
+#include <cupti.h>
+#endif
+#include <array>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <set>
+
+namespace KINETO_NAMESPACE {
+
+using namespace libkineto;
+
+
+/* CuptiCallbackAPI : Provides an abstraction over CUPTI callback
+ *  interface. This enables various callback functions to be registered
+ *  with this class. The class registers a global callback handler that
+ *  redirects to the respective callbacks.
+ *
+ *  Note: one design choice we made is to only support simple function pointers
+ *  in order to speed up the implementation for fast path.
+ */
+
+#ifndef HAS_CUPTI
+enum CUpti_CallbackDomain {
+  CUPTI_CB_DOMAIN_RESOURCE,
+  CUPTI_CB_DOMAIN_RUNTIME_API,
+};
+enum CUpti_CallbackData {
+  CUPTI_RUNTIME_TRACE_CBID_cudaLaunchKernel_v7000,
+  CUPTI_CBID_RESOURCE_CONTEXT_CREATED,
+  CUPTI_CBID_RESOURCE_CONTEXT_DESTROY_STARTING,
+};
+
+using CUpti_CallbackId = size_t;
+#endif
+
+using CuptiCallbackFn = void(*)(
+    CUpti_CallbackDomain domain,
+    CUpti_CallbackId cbid,
+    const CUpti_CallbackData* cbInfo);
+
+
+class CuptiCallbackAPI {
+
+ public:
+
+  /* Global list of supported callback ids
+   *  use the class namespace to avoid confusing with CUPTI enums*/
+  enum CuptiCallBackID {
+    CUDA_LAUNCH_KERNEL =  0,
+    // can possibly support more callback ids per domain
+    //
+    __RUNTIME_CB_DOMAIN_START = CUDA_LAUNCH_KERNEL,
+
+    // Callbacks under Resource CB domain
+    RESOURCE_CONTEXT_CREATED,
+    RESOURCE_CONTEXT_DESTROYED,
+
+    __RUNTIME_CB_DOMAIN_END = RESOURCE_CONTEXT_CREATED,
+    __RESOURCE_CB_DOMAIN_START = RESOURCE_CONTEXT_CREATED,
+
+    __RESOURCE_CB_DOMAIN_END = RESOURCE_CONTEXT_DESTROYED + 1,
+  };
+
+
+  CuptiCallbackAPI(const CuptiCallbackAPI&) = delete;
+  CuptiCallbackAPI& operator=(const CuptiCallbackAPI&) = delete;
+
+  static CuptiCallbackAPI& singleton();
+
+  bool initSuccess() const {
+    return initSuccess_;
+  }
+
+#ifdef HAS_CUPTI
+  CUptiResult getCuptiStatus() const {
+    return lastCuptiStatus_;
+  }
+#endif
+
+  bool registerCallback(
+    CUpti_CallbackDomain domain,
+    CuptiCallBackID cbid,
+    CuptiCallbackFn cbfn);
+
+  // returns false if callback was not found
+  bool deleteCallback(
+    CUpti_CallbackDomain domain,
+    CuptiCallBackID cbid,
+    CuptiCallbackFn cbfn);
+
+  bool enableCallback(CUpti_CallbackDomain domain, CUpti_CallbackId cbid);
+  bool disableCallback(CUpti_CallbackDomain domain, CUpti_CallbackId cbid);
+
+
+  // Please do not use this method. This has to be exposed as public
+  // so it is accessible from the callback handler
+  void __callback_switchboard(
+      CUpti_CallbackDomain domain,
+      CUpti_CallbackId cbid,
+      const CUpti_CallbackData* cbInfo);
+
+ private:
+
+  explicit CuptiCallbackAPI();
+
+  // For callback table design overview see the .cpp file
+  using CallbackList = std::list<CuptiCallbackFn>;
+
+  // level 2 tables sizes are known at compile time
+  constexpr static size_t RUNTIME_CB_DOMAIN_SIZE
+    = (__RUNTIME_CB_DOMAIN_END - __RUNTIME_CB_DOMAIN_START);
+
+  constexpr static size_t RESOURCE_CB_DOMAIN_SIZE
+    = (__RESOURCE_CB_DOMAIN_END - __RESOURCE_CB_DOMAIN_START);
+
+  // level 1 table is a struct
+  struct CallbackTable {
+    std::array<CallbackList, RUNTIME_CB_DOMAIN_SIZE> runtime;
+    std::array<CallbackList, RESOURCE_CB_DOMAIN_SIZE> resource;
+
+    CallbackList* lookup(CUpti_CallbackDomain domain, CuptiCallBackID cbid);
+  };
+
+  CallbackTable callbacks_;
+  bool initSuccess_ = false;
+
+#ifdef HAS_CUPTI
+  CUptiResult lastCuptiStatus_;
+  CUpti_SubscriberHandle subscriber_;
+#endif
+};
+
+} // namespace KINETO_NAMESPACE
---
+++ libkineto/test/CuptiCallbackAPITest.cpp
@@ -0,0 +1,244 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "src/Logger.h"
+#include "src/CuptiCallbackAPI.h"
+
+#include <gtest/gtest.h>
+#include <atomic>
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono;
+using namespace KINETO_NAMESPACE;
+using namespace libkineto;
+
+const size_t some_data = 42;
+
+std::atomic<int> simple_cb_calls = 0;
+
+void simple_cb(
+    CUpti_CallbackDomain domain,
+    CUpti_CallbackId cbid,
+    const CUpti_CallbackData* cbInfo) {
+
+  // simple arg check
+  EXPECT_EQ(domain, CUPTI_CB_DOMAIN_RUNTIME_API);
+  EXPECT_EQ(cbid, CUPTI_RUNTIME_TRACE_CBID_cudaLaunchKernel_v7000);
+  EXPECT_EQ(*reinterpret_cast<const size_t*>(cbInfo), some_data);
+
+  simple_cb_calls++;
+}
+
+void atomic_cb(
+    CUpti_CallbackDomain /*domain*/,
+    CUpti_CallbackId /*cbid*/,
+    const CUpti_CallbackData* /*cbInfo)*/) {
+  // do some atomics in a loop
+  for (int i = 0; i < 1000; i++) {
+    // would have used release consistency but this is fine
+    simple_cb_calls++;
+  }
+}
+
+void empty_cb(
+    CUpti_CallbackDomain /*domain*/,
+    CUpti_CallbackId /*cbid*/,
+    const CUpti_CallbackData* /*cbInfo*/) {
+}
+
+TEST(CuptiCallbackAPITest, SimpleTest) {
+  auto& api = CuptiCallbackAPI::singleton();
+
+  auto addSimpleCallback = [&]() -> bool {
+    bool ret = api.registerCallback(
+        CUPTI_CB_DOMAIN_RUNTIME_API,
+        CuptiCallbackAPI::CUDA_LAUNCH_KERNEL,
+        &simple_cb
+    );
+    return ret;
+  };
+  EXPECT_TRUE(addSimpleCallback()) << "Failed to add callback";
+
+  // duplicate add should be okay
+  EXPECT_TRUE(addSimpleCallback()) << "Failed to re-add callback";
+
+  simple_cb_calls = 0;
+
+  // simulate callback
+  api.__callback_switchboard(
+      CUPTI_CB_DOMAIN_RUNTIME_API,
+      CUPTI_RUNTIME_TRACE_CBID_cudaLaunchKernel_v7000,
+      reinterpret_cast<const CUpti_CallbackData*>(&some_data));
+
+  EXPECT_EQ(simple_cb_calls, 1);
+
+  bool ret = api.deleteCallback(
+      CUPTI_CB_DOMAIN_RUNTIME_API,
+      CuptiCallbackAPI::CUDA_LAUNCH_KERNEL,
+      &simple_cb
+  );
+
+  EXPECT_TRUE(ret) << "Failed to remove callback";
+
+  ret = api.deleteCallback(
+      CUPTI_CB_DOMAIN_RUNTIME_API,
+      CuptiCallbackAPI::CUDA_LAUNCH_KERNEL,
+      &atomic_cb
+  );
+
+  EXPECT_FALSE(ret) << "oops! deleted a callback that was never added";
+}
+
+TEST(CuptiCallbackAPITest, AllCallbacks) {
+  auto& api = CuptiCallbackAPI::singleton();
+
+  auto testCallback = [&](
+      CUpti_CallbackDomain domain,
+      CUpti_CallbackId cbid,
+      CuptiCallbackAPI::CuptiCallBackID kineto_cbid) -> bool {
+
+    bool ret = api.registerCallback(domain, kineto_cbid, atomic_cb);
+    EXPECT_TRUE(ret) << "Failed to add callback";
+
+    if (!ret) {
+      return false;
+    }
+
+    simple_cb_calls = 0;
+    api.__callback_switchboard(domain, cbid, nullptr);
+    EXPECT_EQ(simple_cb_calls, 1000);
+    ret = simple_cb_calls == 1000;
+
+    EXPECT_TRUE(api.deleteCallback(domain, kineto_cbid, atomic_cb));
+
+    return ret;
+  };
+
+  EXPECT_TRUE(
+      testCallback(
+        CUPTI_CB_DOMAIN_RESOURCE,
+        CUPTI_CBID_RESOURCE_CONTEXT_CREATED,
+        CuptiCallbackAPI::RESOURCE_CONTEXT_CREATED))
+    << "Failed to run callback for RESOURCE_CONTEXT_CREATED";
+
+  EXPECT_TRUE(
+      testCallback(
+        CUPTI_CB_DOMAIN_RESOURCE,
+        CUPTI_CBID_RESOURCE_CONTEXT_DESTROY_STARTING,
+        CuptiCallbackAPI::RESOURCE_CONTEXT_DESTROYED))
+    << "Failed to run callback for RESOURCE_CONTEXT_DESTROYED";
+
+  EXPECT_TRUE(
+      testCallback(
+        CUPTI_CB_DOMAIN_RUNTIME_API,
+        CUPTI_RUNTIME_TRACE_CBID_cudaLaunchKernel_v7000,
+        CuptiCallbackAPI::CUDA_LAUNCH_KERNEL))
+    << "Failed to run callback for CUDA_LAUNCH_KERNEL";
+
+}
+
+TEST(CuptiCallbackAPITest, ContentionTest) {
+  auto& api = CuptiCallbackAPI::singleton();
+  const CUpti_CallbackDomain domain = CUPTI_CB_DOMAIN_RUNTIME_API;
+  const CUpti_CallbackId cbid = CUPTI_RUNTIME_TRACE_CBID_cudaLaunchKernel_v7000;
+  const CuptiCallbackAPI::CuptiCallBackID kineto_cbid =
+    CuptiCallbackAPI::CUDA_LAUNCH_KERNEL;
+
+  bool ret = api.registerCallback(domain, kineto_cbid, empty_cb);
+  EXPECT_TRUE(ret) << "Failed to add callback";
+
+  const int iters = 10000;
+  const int num_readers = 8;
+
+  simple_cb_calls = 0;
+
+  // simulate callbacks being executed on multiple threads in parallel
+  //  during this interval add a new atomic_callback.
+  //  this test ensured mutual exclusion is working fine
+  auto read_fn = [&](int tid){
+    auto start_ts = high_resolution_clock::now();
+    for (int i = 0; i < iters; i++) {
+      api.__callback_switchboard(domain, cbid, nullptr);
+    }
+    auto runtime_ms = duration_cast<milliseconds>(
+      high_resolution_clock::now() - start_ts);
+    LOG(INFO) << "th " << tid << " done in " << runtime_ms.count() << " ms";
+  };
+
+
+  std::vector<std::thread> read_ths;
+  for (int i = 0; i< num_readers; i++) {
+    read_ths.emplace_back(read_fn, i);
+  }
+
+  ret = api.registerCallback(domain, kineto_cbid, atomic_cb);
+  EXPECT_TRUE(ret) << "Failed to add callback";
+
+  for (auto& t : read_ths) {
+    t.join();
+  }
+
+  //EXPECT_GT(simple_cb_calls, 0)
+  //  << "Atomic callback should have been called at least once.";
+
+  api.deleteCallback(domain, kineto_cbid, empty_cb);
+  api.deleteCallback(domain, kineto_cbid, atomic_cb);
+}
+
+TEST(CuptiCallbackAPITest, Bechmark) {
+
+  constexpr int iters = 1000;
+  // atomic bench a number of times to get a baseline
+
+  const CUpti_CallbackDomain domain = CUPTI_CB_DOMAIN_RUNTIME_API;
+  const CUpti_CallbackId cbid = CUPTI_RUNTIME_TRACE_CBID_cudaLaunchKernel_v7000;
+  const CuptiCallbackAPI::CuptiCallBackID kineto_cbid =
+    CuptiCallbackAPI::CUDA_LAUNCH_KERNEL;
+
+  LOG(INFO) << "Iteration count = " << iters;
+
+  const bool use_empty = true;
+  auto cbfn = use_empty ? &empty_cb : &atomic_cb;
+
+  // warmup
+  for (int i = 0; i < 50; i++) {
+    (*cbfn)(domain, cbid, nullptr);
+  }
+
+  auto start_ts = high_resolution_clock::now();
+  for (int i = 0; i < iters; i++) {
+    (*cbfn)(domain, cbid, nullptr);
+  }
+  auto delta_baseline_ns = duration_cast<nanoseconds>(
+      high_resolution_clock::now() - start_ts);
+  LOG(INFO) << "Baseline runtime  = " << delta_baseline_ns.count() << " ns";
+
+
+  auto& api = CuptiCallbackAPI::singleton();
+  bool ret = api.registerCallback(domain, kineto_cbid, cbfn);
+  EXPECT_TRUE(ret) << "Failed to add callback";
+
+  // warmup
+  for (int i = 0; i < 50; i++) {
+    api.__callback_switchboard(domain, cbid, nullptr);
+  }
+
+  start_ts = high_resolution_clock::now();
+  for (int i = 0; i < iters; i++) {
+    api.__callback_switchboard(domain, cbid, nullptr);
+  }
+
+  auto delta_callback_ns = duration_cast<nanoseconds>(
+      high_resolution_clock::now() - start_ts);
+  LOG(INFO) << "Callback runtime  = " << delta_callback_ns.count() << " ns";
+
+  LOG(INFO) << "Callback runtime per iteration = " <<
+    (delta_callback_ns.count() - delta_baseline_ns.count()) / (double) iters
+    << " ns";
+
+}

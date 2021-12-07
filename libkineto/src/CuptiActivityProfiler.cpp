730b7aaebee50e0e63237f3a8545e305da96f393
Aaron Shi <aaronshi@fb.com>
2021-12-07 12:09:53 -0800

    Implement USTLoggerCollector to log errors to Scuba
    Summary:
    This patch adds a new LoggerObserver, USTLoggerCollector
    which will collect all error logs and push them to UST Logger.
    The pushes to UST Logger will be separated into 3 parts:
    Warm up, Collection, and Post Processing. These help collect
    success and failure rates in Kineto on a fleetwide basis. The
    pushes can be found on the Scuba Table, Unified System
    Traces Sandbox.
    
    Reviewed By: chaekit
    
    Differential Revision: D32605083
    
    fbshipit-source-id: 93ce43ce5907a4636b3e4ceaa81b5e73ec439028

--- libkineto/src/CuptiActivityProfiler.cpp
+++ libkineto/src/CuptiActivityProfiler.cpp
@@ -403,12 +403,6 @@ void CuptiActivityProfiler::configure(
     return;
   }
 
-#if !USE_GOOGLE_LOG
-  // Add a LoggerObserverCollector to collect all logs during the trace.
-  loggerCollectorMetadata_ = std::make_unique<LoggerCollector>();
-  Logger::addLoggerObserver(loggerCollectorMetadata_.get());
-#endif // !USE_GOOGLE_LOG
-
   config_ = config.clone();
 
   if (config_->activitiesDuration().count() == 0) {
@@ -436,6 +430,12 @@ void CuptiActivityProfiler::configure(
 
   // Ensure we're starting in a clean state
   resetTraceData();
+
+#if !USE_GOOGLE_LOG
+  // Add a LoggerObserverCollector to collect all logs during the trace.
+  loggerCollectorMetadata_ = std::make_unique<LoggerCollector>();
+  Logger::addLoggerObserver(loggerCollectorMetadata_.get());
+#endif // !USE_GOOGLE_LOG
 
 #if defined(HAS_CUPTI) || defined(HAS_ROCTRACER)
   if (!cpuOnly_) {
@@ -555,6 +555,8 @@ const time_point<system_clock> CuptiActivityProfiler::performRunLoopStep(
 #endif // HAS_CUPTI || HAS_ROCTRACER
 
       if (now >= profileStartTime_) {
+        // This line supports the UST Logger. TODO: wrap semantics.
+        LOG(INFO) << "Completed Stage: " << kWarmUpStage;
         if (now > profileStartTime_ + milliseconds(10)) {
           LOG(WARNING)
               << "Tracing started "
@@ -602,6 +604,9 @@ const time_point<system_clock> CuptiActivityProfiler::performRunLoopStep(
         std::lock_guard<std::mutex> guard(mutex_);
         stopTraceInternal(now);
         VLOG_IF(0, now >= profileEndTime_) << "Reached profile end time";
+
+        // This line supports the UST Logger. TODO: wrap semantics.
+        LOG(INFO) << "Completed Stage: " << kCollectionStage;
       } else if (now < profileEndTime_ && profileEndTime_ < nextWakeupTime) {
         new_wakeup_time = profileEndTime_;
       }
@@ -614,6 +619,8 @@ const time_point<system_clock> CuptiActivityProfiler::performRunLoopStep(
       // for quickly handling trace request via synchronous API
       std::lock_guard<std::mutex> guard(mutex_);
       processTraceInternal(*logger_);
+      // This line supports the UST Logger. TODO: wrap semantics.
+      LOG(INFO) << "Completed Stage: " << kPostProcessingStage;
       resetInternal();
       VLOG(0) << "ProcessTrace -> WaitForRequest";
       break;

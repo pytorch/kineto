6159b9d9dc3b2a8cb19aecdaac7257cf8ac62565
guyang3532 <62738430+guyang3532@users.noreply.github.com>
2022-02-08 18:15:12 +0800

    install torchvision from nightly (#519)

--- tb_plugin/ci_scripts/install_env.sh
+++ tb_plugin/ci_scripts/install_env.sh
@@ -25,6 +25,10 @@ if [ "$PYTORCH_VERSION" = "nightly" ]; then
     pip install --pre torch -f "https://download.pytorch.org/whl/nightly/$CUDA_VERSION/torch_nightly.html"
     pip install --pre torchvision --no-deps -f "https://download.pytorch.org/whl/nightly/$CUDA_VERSION/torch_nightly.html"
 elif [ "$PYTORCH_VERSION" = "1.11rc" ]; then
+    pip install --pre torch -f "https://download.pytorch.org/whl/test/$CUDA_VERSION/torch_test.html"
+    #pip install --pre torchvision --no-deps -f "https://download.pytorch.org/whl/test/$CUDA_VERSION/torch_test.html"
+    pip install --pre torchvision --no-deps -f "https://download.pytorch.org/whl/nightly/$CUDA_VERSION/torch_nightly.html"
+elif [ "$PYTORCH_VERSION" = "stable" ]; then
     pip install torch torchvision
 fi
 

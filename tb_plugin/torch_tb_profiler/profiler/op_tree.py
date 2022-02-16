34ebde881140276b0a04ade209f6bae81e742c46
Mike Guo <myguo@microsoft.com>
2022-02-16 16:45:35 +0800

    change log verbosity

--- tb_plugin/torch_tb_profiler/profiler/op_tree.py
+++ tb_plugin/torch_tb_profiler/profiler/op_tree.py
@@ -32,7 +32,7 @@ class OpTreeBuilder:
 
         # if could not find any forward/backward association, skip the processing
         if not fwd_bwd_map:
-            logger.warning('there is no any forwarwd backward association, skip processing backward correlation.')
+            logger.debug('there is no any forwarwd backward association, skip processing backward correlation.')
             return self.tid2tree
 
         self._set_main_tid()

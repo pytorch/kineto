pytorch/kineto

commit b270ac161c350ea4b23670af388e341939fb8153

Supporting adding addtional string metadata (#601)
Summary:
Pull Request resolved: https://github.com/pytorch/kineto/pull/601

Update chromtrace to support string metadata

Reviewed By: aaronenyeshi

Differential Revision: D36420466

fbshipit-source-id: d1ac18d349810cbb611524aebc6fd990afe7f8f9

libkineto/src/output_json.cpp
@@ -41,10 +41,15 @@ std::string& ChromeTraceLogger::sanitizeStrForJSON(std::string& value) {
 }
 
 void ChromeTraceLogger::metadataToJSON(
-    const std::unordered_map<std::string, std::string>& metadata) {
+    const std::unordered_map<std::string, std::string>& metadata, bool quote) {
   for (const auto& kv : metadata) {
-    traceOf_ << fmt::format(R"JSON(
+    if (quote) {
+      traceOf_ << fmt::format(R"JSON(
+  "{}": "{}",)JSON", kv.first, kv.second);
+    } else {
+      traceOf_ << fmt::format(R"JSON(
   "{}": {},)JSON", kv.first, kv.second);
+    }
   }
 }
 
@@ -60,7 +65,7 @@ void ChromeTraceLogger::handleTraceStart(
   ],)JSON", devicePropertiesJson());
 #endif
 
-  metadataToJSON(metadata);
+  metadataToJSON(metadata, true /*quote*/);
   traceOf_ << R"JSON(
   "traceEvents": [)JSON";
 }

libkineto/src/output_json.h
@@ -75,7 +75,9 @@ class ChromeTraceLogger : public libkineto::ActivityLogger {
 
   void handleGenericLink(const ITraceActivity& activity);
 
-  void metadataToJSON(const std::unordered_map<std::string, std::string>& metadata);
+  void metadataToJSON(
+      const std::unordered_map<std::string, std::string>& metadata,
+      bool quote = false);
 
   std::string& sanitizeStrForJSON(std::string& value);
 

commit 3abc8c73adf2c3210f7001a9bc3ee009c6420916

Fix json quoting issue
Summary:
Fixes an issue introduced in https://github.com/pytorch/kineto/pull/601
We should not be assuming the json data value is string but in general could be any generic json blurb.
We will add the quotes while logging to the metadata string itself.

Reviewed By: robieta

Differential Revision: D36612241

fbshipit-source-id: 2eab9e9cc18af3effc1b62a8dca2df6b1ee1b97c

libkineto/src/output_json.cpp
@@ -41,15 +41,10 @@ std::string& ChromeTraceLogger::sanitizeStrForJSON(std::string& value) {
 }
 
 void ChromeTraceLogger::metadataToJSON(
-    const std::unordered_map<std::string, std::string>& metadata, bool quote) {
+    const std::unordered_map<std::string, std::string>& metadata) {
   for (const auto& kv : metadata) {
-    if (quote) {
-      traceOf_ << fmt::format(R"JSON(
-  "{}": "{}",)JSON", kv.first, kv.second);
-    } else {
-      traceOf_ << fmt::format(R"JSON(
+    traceOf_ << fmt::format(R"JSON(
   "{}": {},)JSON", kv.first, kv.second);
-    }
   }
 }
 
@@ -65,7 +60,7 @@ void ChromeTraceLogger::handleTraceStart(
   ],)JSON", devicePropertiesJson());
 #endif
 
-  metadataToJSON(metadata, true /*quote*/);
+  metadataToJSON(metadata);
   traceOf_ << R"JSON(
   "traceEvents": [)JSON";
 }

libkineto/src/output_json.h
@@ -76,8 +76,7 @@ class ChromeTraceLogger : public libkineto::ActivityLogger {
   void handleGenericLink(const ITraceActivity& activity);
 
   void metadataToJSON(
-      const std::unordered_map<std::string, std::string>& metadata,
-      bool quote = false);
+      const std::unordered_map<std::string, std::string>& metadata);
 
   std::string& sanitizeStrForJSON(std::string& value);
 
438392ceeb34a9462057f5910df3cc18586dcd25
Aaron Enye Shi <enye.shi@gmail.com>
2022-06-27 13:42:37 -0700

    Fix LICENSE and Copyright Headers for files in OSS Kineto
    Summary:
    Follow the BSD License OSS guidance, and fix the copyright headers. Remove `Confidential and proprietary.` and `All rights reserved.` and replace with BSD license.
    
    Also, replace Facebook with `Meta Platforms, Inc. and affiliates`.
    
    Reviewed By: yns88, jurajh-fb
    
    Differential Revision: D37467493
    
    Pulled By: aaronenyeshi
    
    fbshipit-source-id: 34d1be499f5cac7261ee8078427e5ebc663755c1

--- libkineto/src/CudaDeviceProperties.cpp
+++ libkineto/src/CudaDeviceProperties.cpp
@@ -1,9 +1,8 @@
-/*
- * Copyright (c) Kineto Contributors
- * All rights reserved.
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 
 #include "CudaDeviceProperties.h"
 

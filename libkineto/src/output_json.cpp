c4597ab3aa196fe416dc819a32bed96cfb3a3afc
Jay Chae <jchae@fb.com>
2022-08-11 23:42:24 -0700

    guard against negative durations in Chrome trace generation (#646)
    Summary:
    Pull Request resolved: https://github.com/pytorch/kineto/pull/646
    
    the profiler may have 0 end time stamp which was recently fixed in D38426342. even though thats the source of negative duration, we should be extra cautious with negative duration in chrome trace generation as thats where the side effect is the worst: super zoomed out trace that is annoying
    
    Reviewed By: aaronenyeshi
    
    Differential Revision: D38607705
    
    fbshipit-source-id: 146319395c4534ed532bfa0e9fe5a06d699331ef

--- libkineto/src/output_json.cpp
+++ libkineto/src/output_json.cpp
@@ -264,6 +264,14 @@ void ChromeTraceLogger::handleActivity(
 
   int64_t ts = op.timestamp();
   int64_t duration = op.duration();
+
+  if (duration < 0) {
+    // This should never happen but can occasionally suffer from regression in handling incomplete events.
+    // Having negative duration in Chrome trace can yield in very poor experience so add an extra guard
+    // before we generate trace events.
+    duration = 0;
+  }
+
   if (op.type() ==  ActivityType::GPU_USER_ANNOTATION) {
     // The GPU user annotations start at the same time as the
     // first associated GPU op. Since they appear later

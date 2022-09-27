bbbf8d542557157798ed0f3ae884374510a22034
Seonglyong Gong <slgong@meta.com>
2022-09-27 14:00:02 -0700

    Revisit python tracing OD flow (#658)
    Summary:
    Pull Request resolved: https://github.com/pytorch/kineto/pull/658
    
    X-link: https://github.com/pytorch/pytorch/pull/85326
    
    - add `set_withstack()`, overriding `ClientInterface`'s no-op funtion.
    - revert `start()` and #ifdef
    
    Reviewed By: chaekit
    
    Differential Revision: D39647074
    
    fbshipit-source-id: a3e6968c184aef2ef0e77334ef7ad950455fd77f

--- libkineto/include/ClientInterface.h
+++ libkineto/include/ClientInterface.h
@@ -15,12 +15,9 @@ class ClientInterface {
   virtual ~ClientInterface() {}
   virtual void init() = 0;
   virtual void warmup(bool setupOpInputsCollection) = 0;
-#ifdef USE_KINETO_MIN_CHANGE
-  virtual void start(bool withStack) = 0;
-#else
   virtual void start() = 0;
-#endif
   virtual void stop() = 0;
+  virtual void set_withstack(bool withStack) = 0;
 };
 
 } // namespace libkineto

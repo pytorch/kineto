531ee08a710c068f53eb31d0ce9c9e9070949c18
Dmytro Dzhulgakov <dima.v.dzhulgakov@gmail.com>
2023-01-19 05:40:18 +0000

    Fix uuid generation for IPC fabric

--- libkineto/src/IpcFabricConfigClient.cpp
+++ libkineto/src/IpcFabricConfigClient.cpp
@@ -22,37 +22,37 @@
 namespace KINETO_NAMESPACE {
 
 namespace uuid {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<> dis(0, 15);
-    static std::uniform_int_distribution<> dis2(8, 11);
+std::string generate_uuid_v4() {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  static std::uniform_int_distribution<> dis(0, 15);
+  static std::uniform_int_distribution<> dis2(8, 11);
 
-    std::string generate_uuid_v4() {
-        std::stringstream ss;
-        int i;
-        ss << std::hex;
-        for (i = 0; i < 8; i++) {
-            ss << dis(gen);
-        }
-        ss << "-";
-        for (i = 0; i < 4; i++) {
-            ss << dis(gen);
-        }
-        ss << "-4";
-        for (i = 0; i < 3; i++) {
-            ss << dis(gen);
-        }
-        ss << "-";
-        ss << dis2(gen);
-        for (i = 0; i < 3; i++) {
-            ss << dis(gen);
-        }
-        ss << "-";
-        for (i = 0; i < 12; i++) {
-            ss << dis(gen);
-        };
-        return ss.str();
-    }
+  std::stringstream ss;
+  int i;
+  ss << std::hex;
+  for (i = 0; i < 8; i++) {
+      ss << dis(gen);
+  }
+  ss << "-";
+  for (i = 0; i < 4; i++) {
+      ss << dis(gen);
+  }
+  ss << "-4";
+  for (i = 0; i < 3; i++) {
+      ss << dis(gen);
+  }
+  ss << "-";
+  ss << dis2(gen);
+  for (i = 0; i < 3; i++) {
+      ss << dis(gen);
+  }
+  ss << "-";
+  for (i = 0; i < 12; i++) {
+      ss << dis(gen);
+  };
+  return ss.str();
+}
 }
 
 // Connect to the Dynolog service through Fabric name `dynolog`

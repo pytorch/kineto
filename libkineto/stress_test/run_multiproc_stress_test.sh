5f6c4053f10b471408acd01a6f5495e5118e47f5
valentinandrei <23515689+valentinandrei@users.noreply.github.com>
2023-03-09 11:52:17 -0800

    Increase NCCL primitives frequency
    Summary: This diff modifies the configuration file to increase the frequency of NCCL events.
    
    Differential Revision: D43954532
    
    fbshipit-source-id: 02c3edc818f8c5c15b6bc8d2fdffc1e6fb3bd7bd

--- libkineto/stress_test/run_multiproc_stress_test.sh
+++ libkineto/stress_test/run_multiproc_stress_test.sh
@@ -5,9 +5,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# buck build -c fbcode.platform=platform009 -c fbcode.static_nccl=1 @mode/opt //kineto/libkineto:kineto_stress_test
+buck build -c fbcode.platform=platform010 -c fbcode.static_nccl=1 @mode/opt //kineto/libkineto:kineto_stress_test
 
-MPIPATH=/usr/local/fbcode/platform009/bin/mpirun
+MPIPATH=/usr/local/fbcode/platform010/bin/mpirun
 BINPATH=~/fbsource/fbcode/buck-out/gen/kineto/libkineto/kineto_stress_test
 NUMRANKS=2
 

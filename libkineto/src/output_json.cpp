f824d9ca7d2a4335850937d4837110b7f6c7860d
Shivam Raikundalia <sraikund@meta.com>
2024-06-06 13:25:12 -0700

    Lower GPU User Annotation Margin (#944)
    Summary:
    
    Users have reported that some GPU events overlap on only a single side. This is partially because we add a margin to our GPU User Annotations so that they don't have the same exact start and end time as other events if there is only one event in a user annotation. We can lower the margin now that we don't have rounding issues.
    
    Reviewed By: aaronenyeshi
    
    Differential Revision: D58254807

--- libkineto/src/output_json.cpp
+++ libkineto/src/output_json.cpp
@@ -320,9 +320,9 @@ void ChromeTraceLogger::handleActivity(
     // The GPU user annotations start at the same time as the
     // first associated GPU op. Since they appear later
     // in the trace file, this causes a visualization issue in Chrome.
-    // Make it start one us (1000 ns) earlier.
-    ts-=1000;
-    duration+=1500; // Still need it to end at the original point rounded up.
+    // Make it start one ns earlier and end 2 ns later.
+    ts-=1;
+    duration+=2; // Still need it to end at the original point rounded up.
   }
 
   std::string arg_values = "";

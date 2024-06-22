48cf19f2e69181374745aa3c735dd27aeaa84559
Xiaodong Wang <xdwang@meta.com>
2024-06-21 23:34:00 -0700

    Remove global using namespace
    Summary:
    Fixing this:
    
    ```
    kineto/libkineto/include/ThreadUtil.h:33:17: error: using namespace directive in global context in header [-Werror,-Wheader-hygiene]
       33 | using namespace libkineto;
          |                 ^
    1 error generated.
    ```
    
    Differential Revision: D58904139

--- libkineto/include/ThreadUtil.h
+++ libkineto/include/ThreadUtil.h
@@ -28,7 +28,3 @@ std::string processName(int32_t pid);
 std::vector<std::pair<int32_t, std::string>> pidCommandPairsOfAncestors();
 
 } // namespace libkineto
-
-#ifdef HAS_ROCTRACER
-using namespace libkineto;
-#endif

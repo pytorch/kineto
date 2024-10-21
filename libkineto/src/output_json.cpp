d2383435467d0e88c50374106a9b8443aac92e4f
fuwenguang <fuwenguang@cambricon.com>
2024-10-21 11:09:24 +0800

    use exclude list

--- libkineto/src/output_json.cpp
+++ libkineto/src/output_json.cpp
@@ -351,11 +351,22 @@ void ChromeTraceLogger::handleActivity(const libkineto::ITraceActivity& op) {
   int external_id = 0;
   if (op.linkedActivity()) {
     external_id = op.linkedActivity()->correlationId();
-  } else if (op.type() == libkineto::ActivityType::CPU_OP ||
-      op.type() == libkineto::ActivityType::CPU_INSTANT_EVENT ||
-      op.type() == libkineto::ActivityType::USER_ANNOTATION ||
-      op.type() == libkineto::ActivityType::PYTHON_FUNCTION) {
-    external_id = op.correlationId();
+  } else {
+    // Some runtime events and kernels may not have a linked activity,
+    // should not set an "External id" for them. Otherwise, these events
+    // may be incorrectly linked to the other external events.
+    static const std::set<libkineto::ActivityType> excludedTypes = {
+      libkineto::ActivityType::GPU_MEMCPY,
+      libkineto::ActivityType::GPU_MEMSET,
+      libkineto::ActivityType::CONCURRENT_KERNEL,
+      libkineto::ActivityType::CUDA_RUNTIME,
+      libkineto::ActivityType::CUDA_DRIVER,
+      libkineto::ActivityType::PRIVATEUSE1_RUNTIME,
+      libkineto::ActivityType::PRIVATEUSE1_DRIVER
+    };
+    if (excludedTypes.find(op.type()) == excludedTypes.end()) {
+      external_id = op.correlationId();
+    }
   }
   std::string arg_values = "";
   if (external_id != 0) {

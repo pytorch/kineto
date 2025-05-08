cd61839152e7f551a9d1013e1f6807c3c73b88f5
Abhinav Singh <abhinavsi@fb.com>
2025-05-08 08:51:05 -0700

    Revert D74278271: Use TSC by default for GPU Only
    Differential Revision:
    D74278271
    
    Original commit changeset: 05029e116c53
    
    Original Phabricator Diff: D74278271
    
    fbshipit-source-id: 82f47f48909c0d3abaf4f7d86a115497ec072df2

--- libkineto/src/CuptiActivityProfiler.cpp
+++ libkineto/src/CuptiActivityProfiler.cpp
@@ -101,20 +101,30 @@ std::function<time_t(approx_time_t)>& get_time_converter() {
   return _time_converter;
 }
 #ifdef HAS_ROCTRACER
-timestamp_t getTimeOffset() {
+timestamp_t getTimeOffset(bool gpuOnly) {
   timespec t1;
-  int64_t t0, t00;
-  t0 = libkineto::getApproximateTime();
-  clock_gettime(CLOCK_MONOTONIC, &t1);
-  t00 = libkineto::getApproximateTime();
+  if (gpuOnly) {
+    timespec t0, t00;
+    clock_gettime(CLOCK_REALTIME, &t0);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    clock_gettime(CLOCK_REALTIME, &t00);
 
-  // Confvert to ns (if necessary)
-  t0 = libkineto::get_time_converter()(t0);
-  t00 = libkineto::get_time_converter()(t00);
+    return (timespec_to_ns(t0) >> 1) + (timespec_to_ns(t00) >> 1) -
+        timespec_to_ns(t1);
+  } else {
+    int64_t t0, t00;
+    t0 = libkineto::getApproximateTime();
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    t00 = libkineto::getApproximateTime();
 
-  // Our stored timestamps (from roctracer and generated) are in
-  // CLOCK_MONOTONIC domain (in ns).
-  return (t0 >> 1) + (t00 >> 1) - timespec_to_ns(t1);
+    // Confvert to ns (if necessary)
+    t0 = libkineto::get_time_converter()(t0);
+    t00 = libkineto::get_time_converter()(t00);
+
+    // Our stored timestamps (from roctracer and generated) are in
+    // CLOCK_MONOTONIC domain (in ns).
+    return (t0 >> 1) + (t00 >> 1) - timespec_to_ns(t1);
+  }
 }
 #endif
 
@@ -391,11 +401,7 @@ void CuptiActivityProfiler::processTraceInternal(ActivityLogger& logger) {
 #ifdef HAS_ROCTRACER
   if (!cpuOnly_) {
     VLOG(0) << "Retrieving GPU activity buffers";
-    if (gpuOnly_) {
-      ApproximateClockToUnixTimeConverter clockConverter;
-      get_time_converter() = clockConverter.makeConverter();
-    }
-    timestamp_t offset = getTimeOffset();
+    timestamp_t offset = getTimeOffset(gpuOnly_);
     cupti_.setTimeOffset(offset);
     const int count = cupti_.processActivities(
         std::bind(

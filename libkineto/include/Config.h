b96c858fd84b95c56b88f29e0e040db6d092163d
Ryan Zhang <ryanzhang@meta.com>
2026-02-03 10:40:11 -0800

    D91430608: [Kineto] Add profiler ExperimentalConfig plumbing to Kineto OD trace (#1236)
    Summary:
    
    There are two fields (capture_overload_names, record_python_gc_info) that are available in auto-trace (under ExperimentalConfig) but not in OD. Since Kineto uses Profiler to capture PyTorch events, these need to be plumbed through to the experimental config field of the Profiler. This diff adds a path to do so. I am introducing a new struct to avoid needing to continuously add experimental config fields to the ClientInterface::prepare() method.
    
    No functional changes as this diff has interface changes blocked behind IFDEF, next diff will add the changes to Profiler in PyTorch.
    
    Differential Revision: D91587687

--- libkineto/include/Config.h
+++ libkineto/include/Config.h
@@ -10,6 +10,7 @@
 
 #include "AbstractConfig.h"
 #include "ActivityType.h"
+#include "ClientInterface.h"
 
 #include <cassert>
 #include <chrono>
@@ -203,6 +204,22 @@ class Config : public AbstractConfig {
 
   [[nodiscard]] bool isWithModulesEnabled() const {
     return enableWithModules_;
+  }
+
+  // Experimental Profiler config fields
+  [[nodiscard]] bool isCaptureOverloadNamesEnabled() const {
+    return enableCaptureOverloadNames_;
+  }
+
+  [[nodiscard]] bool isRecordPythonGCInfoEnabled() const {
+    return enableRecordPythonGCInfo_;
+  }
+
+  [[nodiscard]] ProfilerExperimentalConfig profilerExperimentalConfig() const {
+    return {
+        .captureOverloadNames = enableCaptureOverloadNames_,
+        .recordPythonGCInfo = enableRecordPythonGCInfo_,
+    };
   }
 
   // Trace for this long
@@ -480,6 +497,8 @@ class Config : public AbstractConfig {
   bool enableWithStack_{false};
   bool enableWithFlops_{false};
   bool enableWithModules_{false};
+  bool enableCaptureOverloadNames_{false};
+  bool enableRecordPythonGCInfo_{false};
 
   // Profile for specified iterations and duration
   std::chrono::milliseconds activitiesDuration_;

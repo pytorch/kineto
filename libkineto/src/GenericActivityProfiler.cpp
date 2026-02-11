e140da5630e1d960645f342e0b8df39180f72204
zyzshishui <zyzshishui@gmail.com>
2026-02-11 02:50:04 +0000

    1

--- libkineto/src/GenericActivityProfiler.cpp
+++ libkineto/src/GenericActivityProfiler.cpp
@@ -833,6 +833,10 @@ time_point<system_clock> GenericActivityProfiler::performRunLoopStep(
     case RunloopState::Warmup:
       VLOG(1) << "State: Warmup";
       warmup_done = derivedConfig_->isWarmupDone(now, currentIter);
+      if (!cpuOnly_ && derivedConfig_->isProfilingByIteration() &&
+          currentIter >= 0) {
+        clearGpuActivities();
+      }
       // Flushing can take a while so avoid doing it close to the start time
       if (!cpuOnly_ && currentIter < 0 &&
           (derivedConfig_->isProfilingByIteration() ||

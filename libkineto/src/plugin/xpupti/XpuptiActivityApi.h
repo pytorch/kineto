6ce66520930f73d3dae447bd3cf642ad55d0670a
Miroslaw Oksiucik <miroslaw.oksiucik@intel.com>
2026-03-19 19:22:37 +0200

    Reapply "unformat"
    This reverts commit f87ef7ac29e8a7984279724b656fa865056740ac.

--- libkineto/src/plugin/xpupti/XpuptiActivityApi.h
+++ libkineto/src/plugin/xpupti/XpuptiActivityApi.h
@@ -35,17 +35,15 @@ class XpuptiActivityApi {
   static void pushCorrelationID(int id, CorrelationFlowType type);
   static void popCorrelationID(CorrelationFlowType type);
 
-  void enableXpuptiActivities(
-      const std::set<ActivityType>& selected_activities);
+  void enableXpuptiActivities(const std::set<ActivityType>& selected_activities);
   void disablePtiActivities(const std::set<ActivityType>& selected_activities);
   void clearActivities();
   void flushActivities();
 
   virtual std::unique_ptr<XpuptiActivityBufferMap> activityBuffers();
 
-  virtual const std::pair<int, int> processActivities(
-      XpuptiActivityBufferMap&,
-      std::function<void(const pti_view_record_base*)> handler);
+  virtual const std::pair<int, int> processActivities(XpuptiActivityBufferMap&,
+                                                      std::function<void(const pti_view_record_base*)> handler);
 
  private:
   XpuptiActivityBufferMap allocatedGpuTraceBuffers_;
@@ -53,15 +51,11 @@ class XpuptiActivityApi {
   std::mutex mutex_;
   bool externalCorrelationEnabled_{false};
 
-  int processActivitiesForBuffer(
-      uint8_t* buf,
-      size_t validSize,
-      std::function<void(const pti_view_record_base*)> handler);
+  int processActivitiesForBuffer(uint8_t* buf,
+                                 size_t validSize,
+                                 std::function<void(const pti_view_record_base*)> handler);
   static void bufferRequestedTrampoline(uint8_t** buffer, size_t* size);
-  static void bufferCompletedTrampoline(
-      uint8_t* buffer,
-      size_t size,
-      size_t validSize);
+  static void bufferCompletedTrampoline(uint8_t* buffer, size_t size, size_t validSize);
 
  protected:
   void bufferRequested(uint8_t** buffer, size_t* size);

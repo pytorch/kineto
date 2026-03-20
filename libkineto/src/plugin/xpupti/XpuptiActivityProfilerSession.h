4943d7211220428eba6ce3e3813c10bce21216d3
Miroslaw Oksiucik <miroslaw.oksiucik@intel.com>
2026-03-20 13:00:44 +0200

    Reapply "unformat"
    This reverts commit f55b81c8a7f16105afd5d877ecb97ad3b1b2e1d8.

--- libkineto/src/plugin/xpupti/XpuptiActivityProfilerSession.h
+++ libkineto/src/plugin/xpupti/XpuptiActivityProfilerSession.h
@@ -27,18 +27,15 @@ class XpuptiActivityApi;
 
 using DeviceUUIDsT = std::array<unsigned char, 16>;
 
-class XpuptiActivityProfilerSession
-    : public libkineto::IActivityProfilerSession {
+class XpuptiActivityProfilerSession : public libkineto::IActivityProfilerSession {
  public:
   XpuptiActivityProfilerSession() = delete;
-  XpuptiActivityProfilerSession(
-      XpuptiActivityApi& xpti,
-      const std::string& name,
-      const libkineto::Config& config,
-      const std::set<ActivityType>& activity_types);
+  XpuptiActivityProfilerSession(XpuptiActivityApi& xpti,
+                                const std::string& name,
+                                const libkineto::Config& config,
+                                const std::set<ActivityType>& activity_types);
   XpuptiActivityProfilerSession(const XpuptiActivityProfilerSession&) = delete;
-  XpuptiActivityProfilerSession& operator=(
-      const XpuptiActivityProfilerSession&) = delete;
+  XpuptiActivityProfilerSession& operator=(const XpuptiActivityProfilerSession&) = delete;
 
   ~XpuptiActivityProfilerSession();
 
@@ -49,11 +46,10 @@ class XpuptiActivityProfilerSession
     return errors_;
   };
   void processTrace(ActivityLogger& logger) override;
-  void processTrace(
-      ActivityLogger& logger,
-      libkineto::getLinkedActivityCallback get_linked_activity,
-      int64_t captureWindowStartTime,
-      int64_t captureWindowEndTime) override;
+  void processTrace(ActivityLogger& logger,
+                    libkineto::getLinkedActivityCallback get_linked_activity,
+                    int64_t captureWindowStartTime,
+                    int64_t captureWindowEndTime) override;
   std::unique_ptr<libkineto::DeviceInfo> getDeviceInfo() override {
     return {};
   }
@@ -70,11 +66,9 @@ class XpuptiActivityProfilerSession
   void removeCorrelatedPtiActivities(const ITraceActivity* act1);
   bool outOfRange(const ITraceActivity* act);
   int64_t getMappedQueueId(uint64_t sycl_queue_id);
-  const ITraceActivity* linkedActivity(
-      int32_t correlationId,
-      const std::unordered_map<int64_t, int64_t>& correlationMap);
-  void handleCorrelationActivity(
-      const pti_view_record_external_correlation* correlation);
+  const ITraceActivity* linkedActivity(int32_t correlationId,
+                                       const std::unordered_map<int64_t, int64_t>& correlationMap);
+  void handleCorrelationActivity(const pti_view_record_external_correlation* correlation);
 
 #if PTI_VERSION_AT_LEAST(0, 11)
   using pti_view_record_api_t = pti_view_record_api;
@@ -85,17 +79,12 @@ class XpuptiActivityProfilerSession
   std::string getApiName(const pti_view_record_api_t* activity);
 
   template <class pti_view_memory_record_type>
-  void handleRuntimeKernelMemcpyMemsetActivities(
-      ActivityType activityType,
-      const pti_view_memory_record_type* activity,
-      ActivityLogger& logger);
+  void handleRuntimeKernelMemcpyMemsetActivities(ActivityType activityType,
+                                                 const pti_view_memory_record_type* activity,
+                                                 ActivityLogger& logger);
 
-  void handleOverheadActivity(
-      const pti_view_record_overhead* activity,
-      ActivityLogger& logger);
-  void handlePtiActivity(
-      const pti_view_record_base* record,
-      ActivityLogger& logger);
+  void handleOverheadActivity(const pti_view_record_overhead* activity, ActivityLogger& logger);
+  void handlePtiActivity(const pti_view_record_base* record, ActivityLogger& logger);
 
   // enumerate XPU Device UUIDs from runtime for once
   void enumDeviceUUIDs();

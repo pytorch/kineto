60751c077930a31bff9a15cc5e1434a2bf78a3a4
Miroslaw Oksiucik <miroslaw.oksiucik@intel.com>
2026-03-27 17:44:46 +0300

    Revert "unformat"
    This reverts commit 2fe719ab3900366ff230794101199d42178a1085.

--- libkineto/src/plugin/xpupti/XpuptiScopeProfilerConfig.h
+++ libkineto/src/plugin/xpupti/XpuptiScopeProfilerConfig.h
@@ -26,7 +26,8 @@ class XpuptiScopeProfilerConfig : public AbstractConfig {
           fallbackProfileStartTime) override {}
 
   static XpuptiScopeProfilerConfig& get(const Config& cfg) {
-    return dynamic_cast<XpuptiScopeProfilerConfig&>(cfg.feature(kXpuptiProfilerConfigName));
+    return dynamic_cast<XpuptiScopeProfilerConfig&>(
+        cfg.feature(kXpuptiProfilerConfigName));
   }
 
   Config& parent() const {
@@ -67,7 +68,8 @@ class XpuptiScopeProfilerConfig : public AbstractConfig {
  private:
   XpuptiScopeProfilerConfig() = delete;
   explicit XpuptiScopeProfilerConfig(Config& parent) : parent_(&parent) {}
-  explicit XpuptiScopeProfilerConfig(const XpuptiScopeProfilerConfig& other) = default;
+  explicit XpuptiScopeProfilerConfig(const XpuptiScopeProfilerConfig& other) =
+      default;
 
   // some defaults will depend on other configuration
   void setDefaults();

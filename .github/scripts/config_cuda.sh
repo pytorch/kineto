386ffba8a8a40c9185c4c27cda19566b39e2b1d4
Scott Schneider <scott.a.s@gmail.com>
2026-05-14 07:21:27 -0700

    Deselect all but the fuzz test

--- .github/scripts/config_cuda.sh
+++ .github/scripts/config_cuda.sh
@@ -35,20 +35,5 @@ export BUILD_TEST=1
 
 # shellcheck disable=SC2034
 DESELECTED_TESTS=(
-  test/profiler/test_memory_profiler.py::TestDataFlow::test_data_flow_graph_complicated
-  test/profiler/test_memory_profiler.py::TestMemoryProfilerE2E::test_categories_e2e_sequential_fwd_bwd
-  test/profiler/test_memory_profiler.py::TestMemoryProfilerE2E::test_categories_e2e_simple_fwd_bwd
-  test/profiler/test_memory_profiler.py::TestMemoryProfilerE2E::test_categories_e2e_simple_fwd_bwd_step
-
-  # https://github.com/pytorch/kineto/issues/1253
-  test/profiler/test_profiler.py::TestProfiler::test_python_gc_event
-
-  # https://github.com/pytorch/kineto/issues/1308
-  test/profiler/test_profiler.py::TestProfiler::test_record_function_fast
-
-  # https://github.com/pytorch/kineto/issues/1319
-  test/profiler/test_profiler.py::TestProfiler::test_schedule_function_count
-
   test/profiler/test_profiler.py::TestExperimentalUtils::test_fuzz_symbolize
-  test/profiler/test_torch_tidy.py::TestTorchTidyProfiler::test_tensorimpl_invalidation_scalar_args
 )

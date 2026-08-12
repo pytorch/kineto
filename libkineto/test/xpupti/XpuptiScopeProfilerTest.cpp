64af6274b7d531a497fa04a71a6ccc8550378e1a
Konrad Drozd <konrad.drozd@intel.com>
2026-08-12 13:44:23 +0300

    Skip PerKernelScope UT

--- libkineto/test/xpupti/XpuptiScopeProfilerTest.cpp
+++ libkineto/test/xpupti/XpuptiScopeProfilerTest.cpp
@@ -119,6 +119,9 @@ void RunTest(std::string_view perKernel, unsigned maxScopes) {
 /////////////////////////////////////////////////////////////////////
 
 TEST_F(XpuptiScopeProfilerTest, PerKernelScope) {
+  GTEST_SKIP() << "PTI reports success but returns no scope metric records, "
+                  "so the expected metric activities are absent: "
+                  "https://github.com/pytorch/kineto/issues/1533";
   RunTest("true", 314);
 }
 
